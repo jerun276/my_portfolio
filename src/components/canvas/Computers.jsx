4466037fb838a6c018fb4acb64d734a0c29a7eee
jerun276 <jerunkingston@gmail.com>
2024-09-09 10:53:44 +0530

    Computer Component Completed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { Suspense } from "react";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={1.75} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[5, 10, 5]}
+        angle={Math.PI / 8} 
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.65}
+        position={[0, -3, -1.2]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
