8487e14b4d3f44b054b377c851a532111db7bf8f
jerun276 <jerunkingston@gmail.com>
2024-09-09 11:46:54 +0530

    Hero Section and Computer section completed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/no-unknown-property */
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+// eslint-disable-next-line react/prop-types
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -13,7 +14,7 @@ const Computers = () => {
       <pointLight intensity={1} />
       <spotLight
         position={[5, 10, 5]}
-        angle={Math.PI / 8} 
+        angle={Math.PI / 8}
         penumbra={1}
         intensity={1}
         castShadow
@@ -21,8 +22,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.65}
-        position={[0, -3, -1.2]}
+        scale={isMobile ? 0.5 : 0.65}
+        position={isMobile ? [0, -2, -0.9] : [0, -3, -1.2]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,6 +31,22 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -43,7 +60,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
